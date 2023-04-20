bffb0a729b6bd9fb1ba5f08228215497f6e4c909
Kinyua <alfkinyua@gmail.com>
2023-04-20 15:54:03 +0300

    ADd brand styling

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import './features.css';
 
-function Features() {
-  return <div>Features</div>;
+function Features({ title, text }) {
+  return (
+    <div className="gpt3__features section__padding" id="features">
+      <div className="gpt3__features-heading"></div>
+    </div>
+  );
 }
 
 export default Features;
--- src/containers/whatGPT3/WhatGPT.jsx
+++ src/containers/whatGPT3/WhatGPT.jsx
@@ -4,20 +4,29 @@ import Features from '../features/Features';
 
 function WhatGPT() {
   return (
-    <div className="gpt3__whatgpt3 section_margin ">
-      <div className="gpt3__whatgpt3-feature ">
+    <div className="gpt3__whatgpt3 section_margin " id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
         <Features />
       </div>
       <div className="gpt3__whatgpt3-heading">
-        <h1 className="gradient-text">
+        <h1 className="gradient__text">
           The posibbilities are beyond your immagination
         </h1>
         <p>Explore the library</p>
       </div>
       <div className="gpt3__whatgpt3-container">
-        <Features />
-        <Features />
-        <Features />
+        <Features
+          title="Chatbots"
+          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought."
+        />
+        <Features
+          title="Knowledgebase"
+          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+        />
+        <Features
+          title="Education"
+          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+        />
       </div>
     </div>
   );
